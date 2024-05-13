c4207299568228be4f70015ed02aa6d75e66b884
TLLRSK <tullarisk@gmail.com>
2024-05-13 18:05:11 +0200

    changed: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Esto asegura que los módulos TypeScript se resuelvan correctamente
-      'react-dom': 'react-dom',
-      'react-dom/server': 'react-dom/server',
-    },
-  },
-  optimizeDeps: {
-    // Esto asegura que las dependencias de TypeScript sean optimizadas correctamente
-    include: ['@babel/preset-env', 'react', 'react-dom'],
-  },
-  server: {
-    // Esto permite que Vite sirva correctamente las páginas al navegar con rutas React
-    fs: {
-      allow: ['..'],
-    },
-  },
-});
+})
